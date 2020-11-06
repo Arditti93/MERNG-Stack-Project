0f557bb072d80e2c1d7c7fc2fccf72e3c05ce4f7
Alex <alex.arditti@hotmail.com>
2020-11-06 16:35:59 +0000

    AuthContext
    Added AuthContext for login/Logout and responsive menu bar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 // import gql from 'graphql-tag'
+
+
 import { Grid } from 'semantic-ui-react'
 import PostCard from '../components/PostCard'
 import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
+
     const {
         loading,
         data: { getPosts: posts }
