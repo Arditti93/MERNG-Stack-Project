b305ba85d03856299b78ea1cbf9e79b799342660
Alex <alex.arditti@hotmail.com>
2020-11-06 12:10:31 +0000

    register/Login/ClientSide
    Register and Login Users 
    Misc Code clean up & Error Correction

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
+// import gql from 'graphql-tag'
 import { Grid } from 'semantic-ui-react'
 import PostCard from '../components/PostCard'
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
-    const { loading, data: {getPosts: posts} } = useQuery(FETCH_POSTS_QUERY)
+    const {
+        loading,
+        data: { getPosts: posts }
+      } = useQuery(FETCH_POSTS_QUERY);
 
     if(posts){
         console.log(posts)
@@ -18,40 +22,20 @@ function Home() {
             <Grid.Row>
                 {loading ? (
                     <h1>Loading posts...</h1>
-                ): (
+                ) : (
                  posts && 
                  posts.map((post) => (
-                     <Grid.Column key={post.id} style={{marginBottom: 20 }}>
+                     <Grid.Column key={post.id} 
+                     style={{ marginBottom: 20 }}
+                     >
                          <PostCard post={post}/>
                      </Grid.Column>
-                 ))
+                    ))
                 )}
             </Grid.Row>
     </Grid>
     )
 }
-
-const FETCH_POSTS_QUERY = gql`
-    {
-        getPosts{
-            id 
-            body 
-            createdAt 
-            username 
-            likeCount 
-            likes{
-                username
-            }
-            commentCount
-            comments{
-                id 
-                username 
-                createdAt 
-                body
-            }
-        }
-    }
-`
 
 export default Home
 
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,5 +1,4 @@
 const { AuthenticationError, UserInputError } = require('apollo-server');
-const { argsToArgsConfig } = require('graphql/type/definition');
 
 
 const Post = require('../../models/Post')
@@ -33,7 +32,7 @@ module.exports ={
     Mutation: {
         async createPost(_, { body }, context){
             const user = checkAuth(context)
-            if(args.body.trim() === ''){
+            if(body.trim() === ''){
                 throw new Error('Post body must not be empty')
             }
             console.log(user)
